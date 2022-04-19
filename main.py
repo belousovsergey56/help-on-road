363bca5a9570e2f4f12c55280f1b9ea96ee4220d
belousovsergey56 <belousovsergej56@gmail.com>
2022-04-20 00:52:35 +0300

    create project, add templates, static files

---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    with open('data-storage/greeting-price.json') as f:
+        price_list = json.load(f)
+        return render_template('index.html', price_list=price_list)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+@app.route('/feedback')
+def create_feedback():
+    with open('data-storage/feedbacks.json') as file:
+        feedback = json.load(file)
+        return render_template('feedback.html', feedback=feedback)
+
+
+if __name__ == '__main__':
+    app.run(debug='True')
