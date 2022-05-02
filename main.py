65b7719a6c8ca42563df5ce226cad18f6c87419a
belousovsergey56 <belousovsergej56@gmail.com>
2022-05-02 18:24:46 +0300

    add to services logic open-read json file service.json and render ti html

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ def about():
 
 @app.route('/services')
 def services():
-    return render_template('services.html')
+    with open('data-storage/services.json') as file:
+        item = json.load(file)
+        return render_template('services.html', item=item)
 
 
 @app.route('/feedback')
@@ -32,6 +34,9 @@ def create_feedback():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+#TODO
+#автоматизировать дату в футере
 
     
 if __name__ == '__main__':
