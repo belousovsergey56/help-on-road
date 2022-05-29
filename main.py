180e5db0be088a21c9f41777435fce88aa720e5f
belousovsergey56 <belousovsergej56@gmail.com>
2022-05-29 22:23:14 +0300

    add to 'with open' encoding=UTF-8

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def footer_time():
 
 @app.route('/')
 def home():
-    with open('data-storage/greeting-price.json') as f:
+    with open('data-storage/greeting-price.json', encoding='UTF-8') as f:
         master = choice((25, 26, 27, 28, 29, 30))
         price_list = json.load(f)
         return render_template('index.html', price_list=price_list, master=master, year=footer_time(), telephone=contact_data)
@@ -31,25 +31,24 @@ def about():
 
 @app.route('/services')
 def services():
-    with open('data-storage/services.json') as file:
+    with open('data-storage/services.json', encoding='UTF-8') as file:
         item = json.load(file)
         return render_template('services.html', item=item, year=footer_time())
 
 
 @app.route('/feedback')
 def create_feedback():
-    with open('data-storage/feedbacks.json') as file:
+    with open('data-storage/feedbacks.json', encoding='UTF-8') as file:
         feedback = json.load(file)
         return render_template('feedback.html', feedback=feedback, year=footer_time())
 
 
 @app.route('/contact')
 def contact():
-    with open('data-storage/city_area.json') as file:
+    with open('data-storage/city_area.json', encoding='UTF-8') as file:
         area = json.load(file)
     return render_template('contact.html', area=area, year=footer_time(), contact=contact_data)
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run()
