bd07e847c7faf3c727234b5dc17b522e8928565e
belousovsergey56 <belousovsergej56@gmail.com>
2022-07-15 23:11:15 +0300

    add real number, email, telegram. whatsapp

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from random import choice
 
 app = Flask(__name__)
 
-contact_data = {'tel': '+79315787670', 'tg': 'https://t.me/nikname', 'wts': 'https://wa.me/79315787670', 'mail': 'info@svr.ru'}
+contact_data = {'tel': '+79315787670', 'tg': 'https://t.me/svrspb78', 'wts': 'https://wa.me/79315787670', 'mail': 'info@svr-spb.ru'}
 
 def footer_time():
     today = str(datetime.date.today()).split('-')
@@ -18,7 +18,7 @@ def footer_time():
 
 @app.route('/')
 def home():
-    with open('data-storage/greeting-price.json', encoding='UTF-8') as f:
+    with open('/home/c/ck63035/SRV/public_html/data-storage/greeting-price.json', encoding='UTF-8') as f:
         master = choice((25, 26, 27, 28, 29, 30))
         price_list = json.load(f)
         return render_template('index.html', price_list=price_list, master=master, year=footer_time(), telephone=contact_data)
@@ -31,21 +31,21 @@ def about():
 
 @app.route('/services')
 def services():
-    with open('data-storage/services.json', encoding='UTF-8') as file:
+    with open('/home/c/ck63035/SRV/public_html/data-storage/services.json', encoding='UTF-8') as file:
         item = json.load(file)
         return render_template('services.html', item=item, year=footer_time())
 
 
 @app.route('/feedback')
 def create_feedback():
-    with open('data-storage/feedbacks.json', encoding='UTF-8') as file:
+    with open('/home/c/ck63035/SRV/public_html/data-storage/feedbacks.json', encoding='UTF-8') as file:
         feedback = json.load(file)
         return render_template('feedback.html', feedback=feedback, year=footer_time())
 
 
 @app.route('/contact')
 def contact():
-    with open('data-storage/city_area.json', encoding='UTF-8') as file:
+    with open('/home/c/ck63035/SRV/public_html/data-storage/city_area.json', encoding='UTF-8') as file:
         area = json.load(file)
     return render_template('contact.html', area=area, year=footer_time(), contact=contact_data)
 
